0dfe2a8356681b410ad78b02eb2349243350b501
AlfaCefOf <alfaxcef@gmail.com>
2025-12-07 09:38:34 +0500

    adde

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -11,7 +11,6 @@ export const AuthProvider = ({ children }) => {
   useEffect(() => {
     const storedUser = localStorage.getItem('user');
     if (storedUser) {
-      // setUser(JSON.parse(storedUser));
     }
   }, []);
 
